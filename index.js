d1ce7f361a2402b731669620eb26586ef47c8029
Jose Montoya <josem@Joses-MacBook-Pro.local>
2022-06-23 21:02:53 -0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let selectedColor = 'blue'
+
 const painting = document.querySelector('.painting')
 
 painting.addEventListener('click', function(e){
-	console.log(e.target)
+	e.target.style.backgroundColor = 'blue'
 })
 
-
+changeColor.addEventListener('click', function(e)) {
+    pixel
+}
+if(function(e)
+pixel.backgroundColor = 'red'
